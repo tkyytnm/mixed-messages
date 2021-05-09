61f0cafa567ee085e92f3929a87ec5f83bc6313b
Takuyay <mechanicalgiraffe@gmail.com>
2021-05-09 13:51:44 +0900

    create message function

--- script.js
+++ script.js
@@ -1,7 +1,63 @@
-const messages = {
-    colors: ['red', 'blue', 'green', 'yellow', 'orange', 'cyan', 'violet', 'amber', 'aqua', 'gray', 'black', 'white', 'pink'],
-    adjectives: ['bad', 'busy', 'clever', 'crazy', 'cute', 'evil', 'fine', 'funny', 'elegant', 'ugly', 'short', 'long', 'rich', 'shy'],
-    animals: ['dog', 'puppy', 'turtle', 'rabbit', 'parrot', 'cat', 'kitten', 'goldfish', 'mouse', 'tropical fish', 'hamster']
-}
+const randomIndex = (prop) => Math.floor(Math.random() * prop.length);
 
-console.log(`I have a pen.`);
+const messages = {
+  colors: [
+    "red",
+    "blue",
+    "green",
+    "yellow",
+    "orange",
+    "cyan",
+    "violet",
+    "amber",
+    "aqua",
+    "gray",
+    "black",
+    "white",
+    "pink"
+  ],
+  adjectives: [
+    "bad",
+    "busy",
+    "clever",
+    "crazy",
+    "cute",
+    "evil",
+    "fine",
+    "funny",
+    "elegant",
+    "ugly",
+    "short",
+    "long",
+    "rich",
+    "shy"
+  ],
+  animals: [
+    "dog",
+    "puppy",
+    "turtle",
+    "rabbit",
+    "parrot",
+    "cat",
+    "kitten",
+    "goldfish",
+    "mouse",
+    "tropical fish",
+    "hamster"
+  ]
+};
+
+const res = [];
+
+const createMessage = () => {
+  for (let prop in messages) {
+    res.push(messages[prop][randomIndex(prop)]);
+  }
+};
+
+const printMessage = () => {
+  createMessage();
+  console.log(`It's a ${res.join(" ")}!`);
+};
+
+printMessage();
