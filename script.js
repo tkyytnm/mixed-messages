525d9e45fb1c5b87d92552eaf089427f1957c2ec
Takuyay <mechanicalgiraffe@gmail.com>
2021-05-09 13:19:59 +0900

    create massages components

---
+++ script.js
@@ -0,0 +1,7 @@
+const messages = {
+    colors: ['red', 'blue', 'green', 'yellow', 'orange', 'cyan', 'violet', 'amber', 'aqua', 'gray', 'black', 'white', 'pink'],
+    adjectives: ['bad', 'busy', 'clever', 'crazy', 'cute', 'evil', 'fine', 'funny', 'elegant', 'ugly', 'short', 'long', 'rich', 'shy'],
+    animals: ['dog', 'puppy', 'turtle', 'rabbit', 'parrot', 'cat', 'kitten', 'goldfish', 'mouse', 'tropical fish', 'hamster']
+}
+
+console.log(`I have a pen.`);
